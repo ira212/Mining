b19c5b30d6aa46de39017f5d589da4a4e503d3c6
Ira Fay <ira@faygames.com>
2017-10-23 00:52:46 -0400

    Part 1 is done!

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	bool printNow;
+
+	// Use this for initialization
+	void Start () {
+		printNow = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Time.time > 3 && printNow) {
+			print ("It's been 3 seconds!");
+			printNow = false;
+		}
+		
+	}
+}
