f9b496903529ff5a89776d44dc81c85e59a54ed1
Ira Fay <ira@faygames.com>
2017-10-23 01:25:43 -0400

    Part 3

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,6 +7,10 @@ public class GameController : MonoBehaviour {
 	int silver, silverSupply;
 	float mineNow;
 	float miningSpeed;
+	public GameObject cubePrefab;
+	GameObject myCube;
+	Vector3 cubePos;
+	float xPos;
 
 
 	// Use this for initialization
@@ -15,8 +19,9 @@ public class GameController : MonoBehaviour {
 		silver = 0;
 		bronzeSupply = 3;
 		silverSupply = 2;
-		miningSpeed = 3;
+		miningSpeed = 1;
 		mineNow = miningSpeed;
+		xPos = -5;
 	}
 	
 	// Update is called once per frame
@@ -27,10 +32,22 @@ public class GameController : MonoBehaviour {
 			if (bronzeSupply > 0) {
 				bronzeSupply--;
 				bronze++;
+
+				cubePos = new Vector3 (xPos, 0, 0);
+				myCube = Instantiate (cubePrefab, cubePos, Quaternion.identity);
+				myCube.GetComponent<Renderer> ().material.color = Color.red;
+				xPos += 2;
+
 			}
 			else if (silverSupply > 0) {
 				silverSupply--;
 				silver++;
+
+				cubePos = new Vector3 (xPos, 0, 0);
+				myCube = Instantiate (cubePrefab, cubePos, Quaternion.identity);
+				myCube.GetComponent<Renderer> ().material.color = Color.white;
+				xPos += 2;
+
 			}
 
 			print ("Bronze: "+bronze+ " ... Silver: "+silver);
