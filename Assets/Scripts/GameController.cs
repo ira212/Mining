96a390d603f0b8050c3fca8af5d68ea4f92d33c3
Ira Fay <ira@faygames.com>
2017-10-23 02:31:59 -0400

    Final Project

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,37 +3,70 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
-	int bronze, silver, gold;
+	public static int bronze, silver, gold;
+	public static int points, bronzePoints, silverPoints, goldPoints; 
 	float mineNow;
 	float miningSpeed;
 	public GameObject cubePrefab;
 	GameObject myCube;
 	Vector3 cubePos;
 	float xPos, yPos;
-	Color recentCubeColor;
+	Ore recentOre;
 
 
 	// Use this for initialization
 	void Start () {
 		bronze = 0;
 		silver = 0;
-		miningSpeed = 1;
+		miningSpeed = 3;
 		mineNow = miningSpeed;
 		xPos = -5;
 		yPos = -4;
-		recentCubeColor = Color.red;
+		recentOre = Ore.Bronze;
+		bronzePoints = 1;
+		silverPoints = 10;
+		goldPoints = 100;
 	}
 
-	void SpawnCube (Color cubeColor) {
+	public static void ProcessClickedCube (Ore ore) {
+		if (ore == Ore.Bronze) {
+			bronze--;
+			points += bronzePoints;
+		}
+		else if (ore == Ore.Silver) {
+			silver--;
+			points += silverPoints;
+		}
+		else {
+			gold--;
+			points += goldPoints;
+		}
+	}
+
+	void SpawnCube (Ore ore) {
+		Color cubeColor;
+
+		if (ore == Ore.Bronze) {
+			cubeColor = Color.red;
+		}
+		else if (ore == Ore.Silver) {
+			cubeColor = Color.white;
+		}
+		else {
+			cubeColor = Color.yellow;
+		}
+
 		cubePos = new Vector3 (xPos, yPos, 0);
 		myCube = Instantiate (cubePrefab, cubePos, Quaternion.identity);
 		myCube.GetComponent<Renderer> ().material.color = cubeColor;
+		myCube.GetComponent<CubeController> ().myOre = ore;
+
 		xPos += 2;
 		if (xPos > 6) {
 			xPos = -5;
 			yPos += 2; // eventually this will go off screen
 		}
-		recentCubeColor = cubeColor;
+		recentOre = ore;
 	}
 	
 	// Update is called once per frame
@@ -41,20 +74,20 @@ public class GameController : MonoBehaviour {
 		if (Time.time > mineNow) {
 			mineNow += miningSpeed;
 
-			if (bronze == 2 && silver == 2 && recentCubeColor != Color.yellow) {
+			if (bronze == 2 && silver == 2 && recentOre != Ore.Gold) {
 				gold++;
-				SpawnCube (Color.yellow);
+				SpawnCube (Ore.Gold);
 			}
-			else if (bronze < 2) {
+			else if (bronze < 4) {
 				bronze++;
-				SpawnCube (Color.red);
+				SpawnCube (Ore.Bronze);
 			}
 			else {
 				silver++;
-				SpawnCube (Color.white);
+				SpawnCube (Ore.Silver);
 			}
 
-			print ("Bronze: "+bronze+ " ... Silver: "+silver);
+			print ("Total Points: " + points);
 		}
 	}
 }
---
+++ Assets/Scripts/Ore.cs
@@ -0,0 +1,5 @@
+﻿public enum Ore {
+	Bronze,
+	Silver,
+	Gold
+}
