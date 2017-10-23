b503fb0bd36285c36b3dc591d3161687f815c92e
Ira Fay <ira@faygames.com>
2017-10-23 01:06:10 -0400

    Part 2

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,20 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
-	bool printNow;
+	int bronze, bronzeSupply;
+	int silver, silverSupply;
+	float mineNow;
+	float miningSpeed;
+
 
 	// Use this for initialization
 	void Start () {
-		printNow = true;
+		bronze = 0;
+		silver = 0;
+		bronzeSupply = 3;
+		silverSupply = 2;
+		miningSpeed = 3;
+		mineNow = miningSpeed;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (Time.time > mineNow) {
+			mineNow += miningSpeed;
 
-		if (Time.time > 3 && printNow) {
-			print ("It's been 3 seconds!");
-			printNow = false;
+			if (bronzeSupply > 0) {
+				bronzeSupply--;
+				bronze++;
+			}
+			else if (silverSupply > 0) {
+				silverSupply--;
+				silver++;
+			}
+
+			print ("Bronze: "+bronze+ " ... Silver: "+silver);
 		}
-		
 	}
 }
