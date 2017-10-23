394b5446e663b438c93a17d9f96cceb08fc9caca
Ira Fay <ira@faygames.com>
2017-10-23 01:45:46 -0400

    Part 4

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,32 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
-	int bronze, bronzeSupply;
-	int silver, silverSupply;
+	int bronze, silver, gold;
 	float mineNow;
 	float miningSpeed;
 	public GameObject cubePrefab;
 	GameObject myCube;
 	Vector3 cubePos;
-	float xPos;
+	float xPos, yPos;
+	Color recentCubeColor;
 
 
 	// Use this for initialization
 	void Start () {
 		bronze = 0;
 		silver = 0;
-		bronzeSupply = 3;
-		silverSupply = 2;
 		miningSpeed = 1;
 		mineNow = miningSpeed;
 		xPos = -5;
+		yPos = -4;
+		recentCubeColor = Color.red;
 	}
 
 	void SpawnCube (Color cubeColor) {
-		cubePos = new Vector3 (xPos, 0, 0);
+		cubePos = new Vector3 (xPos, yPos, 0);
 		myCube = Instantiate (cubePrefab, cubePos, Quaternion.identity);
 		myCube.GetComponent<Renderer> ().material.color = cubeColor;
 		xPos += 2;
+		if (xPos > 6) {
+			xPos = -5;
+			yPos += 2; // eventually this will go off screen
+		}
+		recentCubeColor = cubeColor;
 	}
 	
 	// Update is called once per frame
@@ -36,16 +41,16 @@ public class GameController : MonoBehaviour {
 		if (Time.time > mineNow) {
 			mineNow += miningSpeed;
 
-			if (bronzeSupply > 0) {
-				bronzeSupply--;
+			if (bronze == 2 && silver == 2 && recentCubeColor != Color.yellow) {
+				gold++;
+				SpawnCube (Color.yellow);
+			}
+			else if (bronze < 2) {
 				bronze++;
-
 				SpawnCube (Color.red);
 			}
-			else if (silverSupply > 0) {
-				silverSupply--;
+			else {
 				silver++;
-
 				SpawnCube (Color.white);
 			}
 
